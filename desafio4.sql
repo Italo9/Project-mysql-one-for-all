a0115143966c04d5b67682f1decdf0a05b2b8bbb
ítalo Lima <contatoitallo@hotmail.com>
2022-06-07 12:39:39 -0300

    Feat: cria uma QUERY que  mostra as pessoas usuárias que estavam ativas no ano de 2021 baseando-se  na data mais recente no histórico de reprodução-REQ 4

--- desafio4.sql
+++ desafio4.sql
@@ -1,13 +1,6 @@
--- SELECT usuarios.nome_usuario AS usuario, IF(YEAR(DATE(reproducao_usuarios.data_reprodu)) = (YEAR(CURRENT_DATE()) -1), 'Usuário ativo', 'Usuário inativo') AS condicao_usuario
--- FROM SpotifyClone.usuarios 
--- INNER JOIN SpotifyClone.reproducao_usuarios 
--- ON usuarios.usuario_id = reproducao_usuarios.usuario_id 
-
--- ORDER BY usuario;
-
--- SELECT usuarios.nome_usuario AS usuario, IF(YEAR(DATE(reproducao_usuarios.data_reprodu)) = (YEAR(CURRENT_DATE()) -1), 'Usuário ativo', 'Usuário inativo') AS condicao_usuario
--- FROM SpotifyClone.usuarios 
--- INNER JOIN SpotifyClone.reproducao_usuarios 
--- ON usuarios.usuario_id = reproducao_usuarios.usuario_id
-
--- ORDER BY usuario;
+SELECT  usuarios.nome_usuario AS usuario,  IF(max(YEAR(DATE(reproducao_usuarios.data_reprodu))) >= '2021' , 'Usuário ativo', 'Usuário inativo') AS condicao_usuario
+FROM SpotifyClone.usuarios 
+INNER JOIN SpotifyClone.reproducao_usuarios 
+ON usuarios.usuario_id = reproducao_usuarios.usuario_id
+GROUP BY usuario
+ORDER BY usuario;
